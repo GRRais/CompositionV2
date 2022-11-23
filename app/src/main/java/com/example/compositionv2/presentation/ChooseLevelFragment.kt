fdac701f47821124926ae33007af7fd74e691a84
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-23 14:08:33 +0300

    finished lesson 15 data binding intro, ready to learn binding adapters

--- app/src/main/java/com/example/compositionv2/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/compositionv2/presentation/ChooseLevelFragment.kt
@@ -6,10 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.compositionv2.R
 import com.example.compositionv2.databinding.FragmentChooseLevelBinding
 import com.example.compositionv2.domain.entity.Level
-
 
 class ChooseLevelFragment : Fragment() {
 
